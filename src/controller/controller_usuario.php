6a9099e2a51d3017abc6c250c7fff066b4403fc4
Gabriel Oliveira <g4briel.jaime.oliveira@gmail.com>
2025-11-24 10:53:52 -0300

    Ensure logout clears session fully

--- src/controller/controller_usuario.php
+++ src/controller/controller_usuario.php
@@ -34,7 +34,16 @@ switch ($opcao) {
 
   case 'sair':
     $_SESSION = [];
-    session_destroy();
+    if (session_id() !== '') {
+      session_unset();
+      session_destroy();
+    }
+
+    if (ini_get('session.use_cookies')) {
+      $params = session_get_cookie_params();
+      setcookie(session_name(), '', time() - 42000, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
+    }
+
     header('Location: ../view/login.php?m=Você%20saiu%20do%20sistema!');
     exit;
 
